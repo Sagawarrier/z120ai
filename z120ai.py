80bef26e9e8ab217b9dbcb761938c5ba771ef8ae
Maximus <maximus@pop-os.localdomain>
2022-05-18 17:22:25 +0200

    I do not have any errors right now, let's go

--- z120ai.py
+++ z120ai.py
@@ -57,8 +57,27 @@ class Activation_ReLu:
 class Activation_Softmax:
     def forward(self, inputs):
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilites
 
+class _Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class _Loss_CategoricalCrossentropy(_Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis = 1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 
 class Sample:
@@ -116,7 +135,7 @@ class Sample:
 
         print(self.activation2.output)
 
- 
+
 def Debug(event):
     print(inputs.data)
     print(len(inputs.data))
@@ -167,3 +186,4 @@ canvas.bind('<B1-Motion>', LineUpdate)
 root.bind('<space>', Batch)
 
 root.mainloop()
+ 
