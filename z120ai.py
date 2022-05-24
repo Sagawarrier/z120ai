7c7951dd1319d4614d98a749f53f343e3e0ad70b
Sagawarrier <eventronb@gmail.com>
2022-05-24 10:26:01 +0200

    I removed the comments

--- z120ai.py
+++ z120ai.py
@@ -1,5 +1,4 @@
 import math
-from re import A
 import numpy as np
 from tkinter import *
 
@@ -80,26 +79,6 @@ class Network:
         self.layer2.weights = layer2weights
         self.layer2.biases = layer2biases
 
-
-
-#class _Loss:
-#    def calculate(self, output, y):
-#        sample_losses = self.forward(output, y)
-#        data_loss = np.mean(sample_losses)
-#        return data_loss
-
-#class _Loss_CategoricalCrossentropy(_Loss):
-#    def forward(self, y_pred, y_true):
-#        samples = len(y_pred)
-#        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
-
-#        if len(y_true.shape) == 1:
-#            correct_confidences = y_pred_clipped[range(samples), y_true]
-#        elif len(y_true.shape) == 2:
-#            correct_confidences = np.sum(y_pred_clipped*y_true, axis = 1)
-
-#        negative_log_likelihoods = -np.log(correct_confidences)
-#        return negative_log_likelihoods
 
 
 def Save(event):
